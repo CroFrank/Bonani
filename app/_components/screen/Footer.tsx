64616369740a0300bb18380e193370a6ca3ba388
Fran <fducekuce@gmail.com>
2023-10-08 15:07:24 +0200

    add link to my portfolio

--- app/_components/screen/Footer.tsx
+++ app/_components/screen/Footer.tsx
@@ -18,10 +18,10 @@ export default function Footer() {
                 </a>
                 <FaWhatsappSquare />
             </div>
-            <div className='flex gap-2 items-center'>
+            <a href="https://crofrank.github.io/portfolio/" className='flex gap-2 items-center' target="_blank">
                 <FaArrowCircleRight />
                 Made by <span className='font-extrabold text-center'>CroFrank</span>
-            </div>
+            </a>
         </div >
     )
 }
